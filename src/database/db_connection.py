6391ad696ec480e8c79676bd5e064bb4e853e475
SonuShaikh <shaikhshahabaj987@gmail.com>
2025-10-28 21:47:55 +1300

    insert code into db

--- src/database/db_connection.py
+++ src/database/db_connection.py
@@ -1,22 +1,46 @@
+# src/database/db_connection.py
+
 import psycopg2
+from psycopg2 import OperationalError
 from config.settings import *
+import logging
 
-# connectivity function
-def db_connection():
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
-    try:
-        conn = psycopg2.connect(
-            dbname      =  DB_NAME,
-            user        =  DB_USER,
-            password    =  DB_PASSWORD,
-            host        =  DB_HOST,
-            port        =  DB_PORT
+class PostgresDB:
+    def __init__(self):
+        self.conn = None
 
-        )
-        print('Database connection successfull!')
-    except Exception as e:
-        print (f'Database connection failed {e}')
+    def connect(self):
+        """Establish a PostgreSQL database connection."""
+        try:
+            self.conn = psycopg2.connect(
+                dbname=DB_NAME,
+                user=DB_USER,
+                password=DB_PASSWORD,
+                host=DB_HOST,
+                port=DB_PORT
+            )
+            logger.info("Database connection successful!")
+            return self.conn
+        except OperationalError as e:
+            logger.info("Database connection failed: {e}")
+            raise
 
+    def get_cursor(self):
+        """Return a cursor object for executing SQL queries."""
+        if not self.conn:
+            self.connect()
+        return self.conn.cursor()
 
-if __name__ == "__main__":
-    db_connection()
+    def close(self):
+        """Close the database connection."""
+        if self.conn:
+            self.conn.close()
+            self.conn = None
+            logger.info("Database connection closed.")
--- src/main.py
+++ src/main.py
@@ -1,14 +1,50 @@
 from src.api_client import WeatherApiClient
+from src.database.db_connection import PostgresDB
+
+
+def insert_city_date(db, data):
+    cursor = db.get_cursor()
+
+    insert_query = """
+        INSERT INTO city_info (city_name, country_code, state_name, latitude, longitude)
+        VALUES (%s, %s, %s, %s, %s)
+        ON CONFLICT DO NOTHING;
+    """
+
+    for record in data:
+        city_name = record.get("name")
+        country_code = record.get("country")
+        state_name = record.get("state", None)
+        latitude = record.get("lat")
+        longitude = record.get("lon")
+
+        cursor.execute(insert_query, (city_name, country_code, state_name, latitude, longitude))
+
+    db.conn.commit()
+    cursor.close()
+    print("City data inserted successfully!")
 
 def main():
-    city = input("Enter the City name").strip()
-    client = WeatherApiClient()
-    data = client.get_weather(city)
+    try:
 
-    if data:
-        print(f"response data {data}")
-    else:
-        print("failed to fetch weather data")
+        city = input("Enter the City name").strip()
+        client = WeatherApiClient()
+        data = client.get_weather(city)
+
+        if data:
+            print(f"response data {data}")
+
+            '''Update data to database'''
+            db = PostgresDB()
+            db.connect()
+            insert_city_date(db, data)
+            db.close()
+        else:
+            print("failed to fetch weather data")
+
+    except Exception as e:
+        print(f"exception {e}")
+
 
 
 if __name__ == "__main__":
